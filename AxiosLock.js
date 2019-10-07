e6c37857acf13364ce4438ba2e2a8d01876fb89c
Tony Duran <yoet92@gmail.com>
2019-10-07 18:02:28 -0300

    AxiosLock

---
+++ AxiosLock.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import UI from 'lockui';
+
+export default function AxiosLock(config, then, error, final){
+    then = then || new Function
+    error = error || new Function
+    final = final || new Function
+    UI.lock();
+    axios(config)
+    .then(res=>{
+      then(res)
+    }).catch(err=>{
+      error(err)
+    }).finally(()=>{
+        final()
+      UI.unlock()
+    })
+  }
